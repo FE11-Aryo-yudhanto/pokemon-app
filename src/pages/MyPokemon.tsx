ba679351603e9c5dfb4afb1e054331df2c678f15
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-13 23:03:14 +0700

    fix layout mypokemon page

--- src/pages/MyPokemon.tsx
+++ src/pages/MyPokemon.tsx
@@ -43,40 +43,42 @@ const MyPokemon = () => {
             confirmButtonText: "Yes",
             cancelButtonColor: "#d33",
             cancelButtonText: "No",
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-              Swal.fire({
-                position: "center",
-                icon: "success",
-                text: "Delete successfully",
-                showConfirmButton: false,
-                timer: 1500,
-              });
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    text: "Delete successfully",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
             }
             navigate(0)
-          })                    
-      }
+        })
+    }
     return (
         <Layout overflow='auto'>
-            <h1 className='text-lg uppercase font-bold text-black text-center pt-5'>My Pokemon</h1>
-            <div className='grid grid-flow-row auto-rows-max grid-cols-2 p-6 '>
-                {
-                    datas.map((data) => (
-                        <Card
-                            onClick={() => detailHandler(data.id, data.name)}
-                            name={data.name}
-                            img={data.id ? (
-                                `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id + ".svg"}`
-                            ) : (
-                                data.url
-                            )}
-                        >
-                            <div className='w-full p-2 flex justify-end text-black' onClick={()=>removeFavMovie(data)}>
-                                <GiCancel size={30}/>
-                            </div>
-                        </Card>
-                    ))
-                }
+            <div className='h-screen overflow-auto'>
+                <h1 className='text-lg uppercase font-bold text-black text-center pt-5'>My Pokemon</h1>
+                <div className='grid grid-flow-row auto-rows-max grid-cols-2 p-6 '>
+                    {
+                        datas.map((data) => (
+                            <Card
+                                onClick={() => detailHandler(data.id, data.name)}
+                                name={data.name}
+                                img={data.id ? (
+                                    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id + ".svg"}`
+                                ) : (
+                                    data.url
+                                )}
+                            >
+                                <div className='w-full p-2 flex justify-end text-black' onClick={() => removeFavMovie(data)}>
+                                    <GiCancel size={30} />
+                                </div>
+                            </Card>
+                        ))
+                    }
+                </div>
             </div>
         </Layout>
     )
