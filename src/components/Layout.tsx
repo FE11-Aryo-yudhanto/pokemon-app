ce41664257291703fb9418d3c7702998ae7b76f5
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-13 22:42:00 +0700

    fix bug overflow uiux components layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,19 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 
 interface LayoutProps {
-    children: React.ReactNode;
-    overflow: string
+    children?: React.ReactNode;
+    overflow?: string
 }
 
 const Layout: FC<LayoutProps> = ({ children, overflow }) => {
     return (
         <div className='flex flex-row justify-center bg-blue-900 h-screen '>
-            <div className={`bg-white w-full md:w-1/2 overflow-${overflow} h-screen`}>
+            {/* <div className={`bg-white w-full md:w-1/2 overflow-${overflow} h-screen`}> */}
+            <div className={`bg-white w-full md:w-1/2 h-screen`}>
                 <Navbar />
-                {children}
+                <div className='h-full overflow-auto'>
+                    {children}
+                </div>
                 <Footer />
             </div>
         </div>
