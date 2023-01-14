4c904f6f09e3adf9d4becb7034518429bb3050fe
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-14 12:55:55 +0700

    added vercel.json

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,9 +10,11 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children, overflow }) => {
     return (
         <div className='flex flex-row justify-center bg-blue-900 h-screen '>
-            <div className={`flex flex-col bg-white dark:bg-base-100 w-full md:w-1/2  h-screen`}>
+            <div className={`flex flex-col bg-white dark:bg-base-100 w-full md:w-1/2`}>
                 <Navbar />
-                <div className={`h-screen overflow-${overflow}`}>{children}</div>
+                <div className={`h-screen overflow-${overflow}`}>
+                    {children}
+                </div>
                 <Footer />
             </div>
         </div>
