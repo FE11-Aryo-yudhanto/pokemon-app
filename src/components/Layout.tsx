b55045e8a0b7de72c884f48dae062eb75e495915
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-14 15:09:29 +0700

    fixed overflow layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,8 +9,8 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children, overflow }) => {
     return (
-        <div className='flex flex-row justify-center bg-blue-900 h-screen overflow-hidden'>
-            <div className={`flex flex-colw-full md:w-1/2`}>
+        <div className='flex flex-row justify-center bg-blue-900 h-screen overflow-auto'>
+            <div className="flex flex-colw-full md:w-1/2">
                 <div className={`h-screen bg-white dark:bg-base-100 overflow-${overflow}`}>
                     <Navbar />
                     {children}
