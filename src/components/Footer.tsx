be73b5bef0e625356c74303e95a1e0f1823e2c68
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-14 13:02:38 +0700

    added new footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,36 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer className="btm-nav flex justify-center w-full sticky bottom-0 z-50 bg-black">
-            <button>
-                <Link to={'/'}>
-                    <TbHome size={30} color='white' />
-                </Link>
-            </button>
-            <button>
-                <Link to={'/mypokemon'}>
-                    <TbPokeball size={30} color='white'/>
-                </Link>
-            </button>
+        // <footer className="btm-nav flex justify-center w-full sticky bottom-0 z-50 bg-black">
+        //     <button>
+        //         <Link to={'/'}>
+        //             <TbHome size={30} color='white' />
+        //         </Link>
+        //     </button>
+        //     <button>
+        //         <Link to={'/mypokemon'}>
+        //             <TbPokeball size={30} color='white'/>
+        //         </Link>
+        //     </button>
+        // </footer>
+        <footer className="footer items-center p-4 bg-black">
+            <div className="items-center grid grid-cols-2 w-full">
+                <div className='flex justify-center w-full'>
+                    <button>
+                        <Link to={'/'}>
+                            <TbHome size={30} color='white' />
+                        </Link>
+                    </button>
+                </div>
+                <div className='flex justify-center w-full'>
+                    <button>
+                        <Link to={'/mypokemon'}>
+                            <TbPokeball size={30} color='white' />
+                        </Link>
+                    </button>
+                </div>
+
+            </div>
         </footer>
     )
 }
